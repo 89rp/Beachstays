30c04ff08304f8955452740c37aaf5667e7da3ee
89rp <rakp.8989@gmail.com>
2022-03-27 15:49:28 -0400

    added comments to js, reorganized to make code functional, increased width of slideInMenu to take up full screen, added menuClose when link is clicked

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".navList");
 
-// update hamburger button class to have postion change to appear on screen
-
+// Storing fucntion in vareiableupdate hamburger button and nav class to add .active which will activate postion change for nav to move on screen
 const slideInMenu = function () {
     hamburger.classList.toggle("active")
     navList.classList.toggle("active")
@@ -11,3 +10,19 @@ const slideInMenu = function () {
 
 // add the event listener to the hamburger button
 hamburger.addEventListener("click", slideInMenu)
+
+
+//storing each link within the nav in a variable.
+const menuLink = document.querySelectorAll(".menuLink");
+
+// Storing fucntion in vareiable to update hamburger button and nav class to remove .active which will activate postion change for nav to move off screen.
+const closeMenu = function() {
+    hamburger.classList.remove("active");
+    navList.classList.remove("active");
+}
+
+// forEach loop that adds eventlistener for when a link in the nav is clicked to call the callback function closeMenu 
+menuLink.forEach(link => link.addEventListener("click", closeMenu));
+
+
+

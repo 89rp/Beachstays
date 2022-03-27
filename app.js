431b64074f75224ed56c9ac61df90ec5b4d0ed31
89rp <rakp.8989@gmail.com>
2022-03-27 01:06:57 -0400

    moved script tag in html to bottom of page, added link class to nav links

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".navList");
 
-// add the event listener to the hamburger button
-hamburger.addEventListener("click", slideInMenu);
-
 // update hamburger button class to have postion change to appear on screen
-function slideInMenu () {
+
+const slideInMenu = function () {
     hamburger.classList.toggle("active")
-    navList.classList.toggle("active");
+    navList.classList.toggle("active")
 }
+
+// add the event listener to the hamburger button
+hamburger.addEventListener("click", slideInMenu)
